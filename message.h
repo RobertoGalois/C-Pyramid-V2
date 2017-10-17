e169a05647767784f7b58a1e14ef4727027a3e8d
RobertoGalois <victor.vague@live.fr>
2017-10-17 20:35:38 +0200

    Create message.h

---
+++ message.h
@@ -0,0 +1,31 @@
+#define     CASSER                  int
+#define     LA                      main
+#define     SALUT_A_TOI             unsigned int    lim;
+#define     TOI_QUI_ME_LIS          short           bool1;
+#define     A_TRAVERS_TON_ECRAN     short           bool2;
+#define     A_TRAVERS_DU_MATERIEL   char            c;
+#define     COMMENT_TU              printf
+#define     VAS                     ("Entrez la taille de la pyramide: ");
+#define     DIS_LE_MOI              bool1 = scanf("%d",&lim);
+#define     DIS_LE_DANS             printf
+#define     LES_COMMENTAIRES        ("Entrez le motif de votre pyramide: ");
+#define     LACHE_UN_POUCE_BLEU     getchar();
+#define     LIKE_ET_PARTAGE         bool2 = scanf("%c",&c);
+#define     SI_TOI_AUSSI_TU_PENSES  if
+#define     QUE_PARFOIS_LES_GENS    (bool1==1 && bool2==1)
+#define     SONT_DURS               {
+#define     BEAUCOUP_TROP_DURS      for(signed int i=0; i<lim; i++)
+#define     ET_BEAUCOUP_TROP        for
+#define     VIOLENTS                (signed int j=0; j<i; j++)
+#define     MET_UN_POUCE_BLEU       putchar(c);
+#define     TU_N_ES                 printf
+#define     PAS_SEUL                ("%c\n",c);
+#define     TU_N_ES_PAS             }
+#define     ANORMAL                 /* non */
+#define     BIZARRE                 /* certainement pas */
+#define     TU_ES_IMPORTANT         else
+#define     CAR_TU_VAS              printf
+#define     PEUT_ETRE_SAUVER        ("Arrete de rentrer des cochonneries !");
+#define     LE_MONDE                /* Merci A Toi Humain Gentil */
+#define     NE_SOIS_PAS_TRISTE      return
+#define     JE_SUIS_AVEC_TOI        (0);
